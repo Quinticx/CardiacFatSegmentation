c1c18fad10f26be80f896ab6a5bc42e4a320659a
Jon Klingensmith <jokling@siue.edu>
2019-11-05 10:38:57 -0600

    reading image and seg nrrds and set up processing as pairs

--- frameAndMaskSourceGen.py
+++ frameAndMaskSourceGen.py
@@ -1,10 +1,30 @@
+import os
 import openpyxl
+import numpy as np
+import nrrd
+
+
+# function to take in nrrd image and seg data and write out individual image filenames
+# - checks to see how many files are already in the folder and uses that as the starting number
+def writeImages(framePath, frameData, segPath, segData):
+
+    # check to see if path exists, and if not create it
+
+    # determine number of slices
+    numSlices = frameData.shape[2]
+    print(numSlices)
+
+    # loop through each slice and write the image file
+
 
 # column numbers for the filenames - 1-based indexing as in excel
-edFileNameCol = 5
-edSegNameCol = 6
-esFileNameCol = 7
-esSegNameCol = 8
+edFileNameCol = 4
+edSegNameCol = 5
+esFileNameCol = 6
+esSegNameCol = 7
+
+# which tissue type for this pass?
+whichTissue = 'LM'  # left myocardium
 
 # top level location of spreadsheet
 spreadSheetName = 'C:\\Users\\jokling\\Documents\\WashU_CCIR_MRIData\\MRI-Table.xlsx'
@@ -20,13 +40,24 @@ worksheet = next(iter(workbook))
 
 # loop through each row and grab out the filenames that are required for both the image data (for the "frames") and
 # the segmentations (for the "masks") - min row is 2 as in excel, to start at first scan
-for row in worksheet.iter_rows(min_row=2, min_col=edFileNameCol, max_col=esSegNameCol):
+for row in worksheet.iter_rows(min_row=2, min_col=1, max_col=8):  # min 1 max 8 for correct column data
 
-    # grab the filenames for this scan
-    edFileName = row[0].value
-    edSegName = row[1].value
-    esFileName = row[2].value
-    esSegName = row[3].value
+    # read the subject ID for this scan
+    subjectID = row[0].value
+    prePostString = row[3].value
 
-    # open each of the NRRD files and "explode" them into separate image files
+    # blank pre/post string means skip this file
+    if prePostString is not None:
 
+        # grab the filenames for this scan
+        edFileName = topLevelDataPath + '\\' + subjectID + '_' + prePostString + '\\' + row[edFileNameCol].value
+        edSegName = topLevelDataPath + '\\' + subjectID + '_' + prePostString + '\\' + row[edSegNameCol].value
+        esFileName = topLevelDataPath + '\\' + subjectID + '_' + prePostString + '\\' + row[esFileNameCol].value
+        esSegName = topLevelDataPath + '\\' + subjectID + '_' + prePostString + '\\' + row[esSegNameCol].value
+
+        # open each of the image NRRD files and "explode" them into separate image files
+        framePath = outputPath + '\\NumberedFrames' + whichTissue
+        segPath = outputPath + '\\NumberedMasks' + whichTissue
+        frameData, frameHeader = nrrd.read(edFileName)
+        segData, segHeader = nrrd.read(edSegName)
+        writeImages(framePath, frameData, segPath, segData)
