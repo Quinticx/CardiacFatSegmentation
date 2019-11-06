551029ba8a199f2529d4243eecc10e0a446c8793
Jon Klingensmith <jokling@siue.edu>
2019-11-06 09:56:06 -0600

    aligning image volumes - interim progress

--- frameAndMaskSourceGen.py
+++ frameAndMaskSourceGen.py
@@ -5,9 +5,63 @@ import nrrd
 import imageio
 import cv2 as cv
 
+
+# take the seg.nrrd input image and pad it based on the extents read from the file for this tissue type
+def padSegImage(inImage, padX, padY):
+
+    # get size of input image
+    sizeInputX, sizeInputY = inImage.shape[1], inImage.shape[0]
+    newSizeX = inImage.shape[1] + padX
+    newSizeY = inImage.shape[0] + padY
+
+    # # create new np array for image data to return
+    # outImage = np.zeros((newSizeY, newSizeX))
+
+    # copy of the image data to the right locations
+
+    outImage = inImage  # pass through
+
+
+    return outImage
+
+
+# takes the headers of both the regular image data and the seg data and computes the padding needed
+# for the alignment of the two volumes - padding will be
+def computeImageAlignment(frameHeader, segHeader):
+
+    # get origin and space directions for each input header
+    frameOrigin = frameHeader.get('space origin')
+    print(frameOrigin)
+    frameSpaceDirections = frameHeader.get('space directions')
+    print(frameSpaceDirections)
+    segOrigin = segHeader.get('space origin')
+    print(segOrigin)
+    segSpaceDirections = segHeader.get('space directions')
+
+    # Remove the channel row from the space directions (it is all NaNs)
+    segSpaceDirections = np.delete(segSpaceDirections, 0, axis=0)
+    print(segSpaceDirections)
+
+    # get spacing for both datasets as single array
+    frameSpacing = np.linalg.norm(frameSpaceDirections, axis=1)
+    print(frameSpacing)
+    segSpacing = np.linalg.norm(segSpaceDirections, axis=1)
+    print(segSpacing)
+
+    # # Handle the space the NRRD file is in
+    # space = self.header.get('space')
+    # if space in ['right-anterior-superior', 'RAS']:
+    #     self.worldSpaceMatrix = np.diag([1, 1, 1])
+    # elif space in ['left-anterior-superior', 'LAS']:
+    #     self.worldSpaceMatrix = np.diag([-1, 1, 1])
+    # elif space in ['left-posterior-superior', 'LPS']:
+    #     self.worldSpaceMatrix = np.diag([-1, -1, 1])
+
+
+
 # function to take in nrrd image and seg data and write out individual image filenames
 # - checks to see how many files are already in the folder and uses that as the starting number
-def writeImages(framePath, frameData, segPath, segData, segHeader, tissueChannel, debugImages):
+def writeImages(framePath, frameData, segPath, segData, segHeader, tissueChannel, extents, debugImages):
 
     # check to see if path exists, and if not create it
     if not os.path.exists(framePath):
@@ -51,21 +105,26 @@ def writeImages(framePath, frameData, segPath, segData, segHeader, tissueChannel
         indexMask = ii
         indexFrame = indexMask + segOffsetFrameIndex
 
-        # extract out the image data for each slice to write to file
-        # imageFrame = frameData[:, :, ii].astype(np.uint8)
-        imageFrame = np.rot90(frameData[:, :, indexFrame])
+        # extract out the image data for each slice to write to file and
+        # adjust orientation to match display in slicer (for debugging)
+        imageFrame = np.fliplr(np.rot90(frameData[:, :, indexFrame]))
 
         # skip this image if all the image slice data is black - check max value below a certain point
         maxImageVal = np.max(imageFrame)
         if maxImageVal < 10:
             continue
 
-        segFrame = (segData[tissueChannel, :, :, indexMask] * 255).astype(np.uint8)  # scale to 0-255
+        # scale to 0-255, convert to int, and adjust orientation
+        segFrame = np.flipud(np.rot90((segData[tissueChannel, :, :, indexMask] * 255).astype(np.uint8)))
 
         # check seg frame data also, if nothing present, skip this one
         maxSegVal = np.max(segFrame)
         if maxSegVal < 10:
             continue
+
+        # now pad the image based on the extents
+        padX, padY = int(extents[0]), int(extents[2])
+        newSegFrame = padSegImage(segFrame, padX, padY)
 
         # setup filenames based on how many images are already in the directory
         imageOutName = '%.3d.png' % (startFileNum + imagesWritten)
@@ -81,14 +140,14 @@ def writeImages(framePath, frameData, segPath, segData, segHeader, tissueChannel
         #     cv.destroyAllWindows()
 
         # write out the images
-        imageio.imwrite(segOutNameFullPath, segFrame)
+        imageio.imwrite(segOutNameFullPath, newSegFrame)
         imageio.imwrite(imageOutNameFullPath, imageFrame)
         imagesWritten = imagesWritten + 1
 
 
 # parses each channel in seg.nrrd and returns the zero-based number of the channel based on matching
 # the name string
-def getTissueChannel(segHeader, whichTissueFullName):
+def getTissueChannelAndExtents(segHeader, whichTissueFullName):
 
     # loop through header information and grab out the number of the tissue type based on the string passed in
     segExists = True
@@ -116,7 +175,11 @@ def getTissueChannel(segHeader, whichTissueFullName):
         else:
             numSeg = numSeg + 1
 
-    return numSeg
+    # use the resulting tissue type to grab the extents and return them
+    extentFieldName = 'Segment%.1d_Extent' % numSeg
+    extentString = segHeader.get(extentFieldName).split(' ')
+
+    return numSeg, extentString
 
 
 # column numbers for the filenames - 1-based indexing as in excel
@@ -167,7 +230,11 @@ for row in worksheet.iter_rows(min_row=2, min_col=1, max_col=8):  # min 1 max 8 
         # temp debug for only MF03PRE
         if subjectID == 'MF0303' and prePostString == 'PRE':
 
-            # get correct tissue "channel" number for this seg file based on which tissue type
-            whichTissueChannel = getTissueChannel(segHeader, whichTissueFullName)
+            # compute the alignment based on image and seg header
+            computeImageAlignment(frameHeader, segHeader)
 
-            writeImages(framePath, frameData, segPath, segData, segHeader, whichTissueChannel, debugImages=True)
+            # get correct tissue "channel" number for this seg file based on which tissue type
+            whichTissueChannel, extents = getTissueChannelAndExtents(segHeader, whichTissueFullName)
+
+            writeImages(framePath, frameData, segPath, segData, segHeader, whichTissueChannel,
+                        extents, debugImages=True)
